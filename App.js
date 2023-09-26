9e68d91971e8e8f9fc7c8da7a3c3f16e24522494
Muskan Jain <muskan611998jain@gmail.com>
2023-09-27 00:49:30 +0530

    episode-3

--- App.js
+++ App.js
@@ -1,18 +1,31 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const parent = React.createElement(
-"div", 
-{id:"parent"}, [
-    React.createElement("div", {id:"child"},[
-        React.createElement("h1", {}, "h1 tag"),
-        React.createElement("h2", {}, "h2 tag")
-]),
-    React.createElement("div", {id:"child2"},[
-        React.createElement("h1", {}, "h1 tag"),
-        React.createElement("h2", {}, "h2 tag")
-    ])
-]);
+const heading = (
+    <h1 className = "head" tabIndex="5">
+        Welcome from React element!
+    </h1>
+);
+
+const Title = () => (
+    <h1 className = "head" tabIndex="5">
+        Welcome!
+    </h1>
+);
+
+/*const HeadingComponent = () => {
+    return <h1>Welcome from functional component</h1>
+}*/
+
+// React functional component
+const HeadingComponent = () => (
+    <div id = "container">
+        <Title />
+        {Title()}
+        {heading}
+        <h1 className="heading">Welcome from functional component</h1>
+    </div>
+);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent);
+root.render(<HeadingComponent />);
